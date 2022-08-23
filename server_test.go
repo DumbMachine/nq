b64e8f057414a3501c5a0a7205d87c1329c4f193
DumbMachine <ratin.kumar.2k@gmail.com>
2022-08-23 20:15:27 +0530

    more tests

--- server_test.go
+++ server_test.go
@@ -3,6 +3,7 @@ package nq
 import (
 	"context"
 	"fmt"
+	"syscall"
 	"testing"
 	"time"
 
@@ -11,39 +12,13 @@ import (
 	natsserver "github.com/nats-io/nats-server/v2/test"
 )
 
-// func TestServerRun(t *testing.T) {
+const (
+	TEST_PORT = 4223
+)
 
-// 	srv := NewServer(NatsClientOpt{
-// 		Addr: nats.DefaultURL,
-// 	}, Config{
-// 		ServerName:  GenerateServerName(),
-// 		Concurrency: 1,
-// 		LogLevel:    InfoLevel,
-// 	},
-// 	)
-
-// 	done := make(chan struct{})
-// 	// Make sure server exits when receiving TERM signal.
-// 	go func() {
-// 		time.Sleep(2 * time.Second)
-// 		syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
-// 		done <- struct{}{}
-// 	}()
-
-// 	go func() {
-// 		select {
-// 		case <-time.After(10 * time.Second):
-// 			panic("server did not stop after receiving TERM signal")
-// 		case <-done:
-// 		}
-// 	}()
-
-// 	if err := srv.Run(); err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
-
-const TEST_PORT = 4223
+var (
+	URL = fmt.Sprintf("nats://127.0.0.1:%d", TEST_PORT)
+)
 
 func RunServerOnPort(port int) *server.Server {
 	opts := natsserver.DefaultTestOptions
@@ -55,75 +30,117 @@ func RunServerWithOptions(opts *server.Options) *server.Server {
 	return natsserver.RunServer(opts)
 }
 
-// func TestServerReconnect(t *testing.T) {
-// 	s := RunServerOnPort(TEST_PORT)
-// 	s.EnableJetStream(&server.JetStreamConfig{})
-// 	go func() {
-// 		s.Start()
-// 	}()
-// 	time.Sleep(time.Second * 1)
-// 	srv := NewServer(NatsClientOpt{
-// 		Addr:          fmt.Sprintf("nats://127.0.0.1:%d", TEST_PORT),
-// 		ReconnectWait: time.Second * 2,
-// 		MaxReconnects: 100,
-// 	}, Config{
-// 		ServerName:  GenerateServerName(),
-// 		Concurrency: 1,
-// 		LogLevel:    InfoLevel,
-// 	},
-// 	)
-// 	srv.Register("test-q", func(ctx context.Context, tp *TaskPayload) error {
-// 		// no-op task function
-// 		return nil
-// 	})
+func TestServerRun(t *testing.T) {
+	s := RunServerOnPort(TEST_PORT)
+	s.EnableJetStream(&server.JetStreamConfig{})
+	defer s.Shutdown()
+	go func() {
+		s.Start()
+	}()
+	time.Sleep(time.Second * 1)
 
-// 	go func() {
-// 		srv.Start()
-// 	}()
+	srv := NewServer(NatsClientOpt{
+		Addr: URL,
+	}, Config{
+		ServerName:  GenerateServerName(),
+		Concurrency: 1,
+		LogLevel:    InfoLevel,
+	},
+	)
+	done := make(chan struct{})
+	// Make sure server exits when receiving TERM signal.
+	go func() {
+		time.Sleep(2 * time.Second)
+		syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+		done <- struct{}{}
+	}()
 
-// 	done := make(chan struct{})
-// 	go func() {
-// 		time.Sleep(2 * time.Second)
-// 		s.Shutdown()
-// 		time.Sleep(2 * time.Second)
-// 		// restart server
-// 		s = RunServerOnPort(TEST_PORT)
-// 		s.EnableJetStream(&server.JetStreamConfig{})
-// 		done <- struct{}{}
-// 	}()
+	go func() {
+		select {
+		case <-time.After(10 * time.Second):
+			panic("server did not stop after receiving TERM signal")
+		case <-done:
+			{
 
-// 	// go func() {
-// 	// defer srv.Shutdown()
-// 	select {
-// 	case <-time.After(10 * time.Second):
-// 		panic("failed to restart nats-server")
-// 	case <-done:
-// 		{
-// 			// check if actually reconnected
-// 			client := NewPublishClient(NatsClientOpt{
-// 				Addr: fmt.Sprintf("nats://127.0.0.1:%d", TEST_PORT),
-// 			})
-// 			defer client.Close()
+			}
+		}
+	}()
 
-// 			task1 := NewTask("test-q", []byte{})
-// 			if ack, err := client.Enqueue(task1); err == nil {
-// 				time.Sleep(time.Second * 2)
-// 				msg, err := client.Fetch(ack.ID)
-// 				if err != nil {
-// 					panic(err)
-// 				}
-// 				if msg.GetStatus() != "completed" {
-// 					panic(fmt.Sprintf("expected=%s found=%s", "completed", msg.GetStatus()))
-// 				}
-// 				srv.Shutdown()
-// 			} else {
-// 				panic("failed to send task")
-// 			}
-// 		}
-// 	}
+	if err := srv.Run(); err != nil {
+		t.Fatal(err)
+	}
 
-// 	defer s.Shutdown()
-// }
+}
+
+func TestServerReconnect(t *testing.T) {
+	s := RunServerOnPort(TEST_PORT)
+	s.EnableJetStream(&server.JetStreamConfig{})
+	go func() {
+		s.Start()
+	}()
+	time.Sleep(time.Second * 1)
+	srv := NewServer(NatsClientOpt{
+		Addr:          URL,
+		ReconnectWait: time.Second * 2,
+		MaxReconnects: 100,
+	}, Config{
+		ServerName:  GenerateServerName(),
+		Concurrency: 1,
+		LogLevel:    InfoLevel,
+	},
+	)
+	srv.Register("test-q", func(ctx context.Context, tp *TaskPayload) error {
+		// no-op task function
+		return nil
+	})
+
+	go func() {
+		srv.Start()
+	}()
+
+	done := make(chan struct{})
+	go func() {
+		time.Sleep(2 * time.Second)
+		s.Shutdown()
+		time.Sleep(2 * time.Second)
+		// restart server
+		s = RunServerOnPort(TEST_PORT)
+		s.EnableJetStream(&server.JetStreamConfig{})
+		done <- struct{}{}
+	}()
+
+	// go func() {
+	// defer srv.Shutdown()
+	select {
+	case <-time.After(10 * time.Second):
+		panic("failed to restart nats-server")
+	case <-done:
+		{
+			// check if actually reconnected
+			client := NewPublishClient(NatsClientOpt{
+				Addr: URL,
+			})
+			defer client.Close()
+
+			task1 := NewTask("test-q", []byte{})
+			if ack, err := client.Enqueue(task1); err == nil {
+				time.Sleep(time.Second * 2)
+				msg, err := client.Fetch(ack.ID)
+				if err != nil {
+					panic(err)
+				}
+				if msg.GetStatus() != "completed" {
+					panic(fmt.Sprintf("expected=%s found=%s", "completed", msg.GetStatus()))
+				}
+				srv.Shutdown()
+			} else {
+				panic("failed to send task")
+			}
+		}
+	}
+
+	defer s.Shutdown()
+}
 
 func TestServerDie(t *testing.T) {
 	s := RunServerOnPort(TEST_PORT)
@@ -135,7 +152,7 @@ func TestServerDie(t *testing.T) {
 
 	time.Sleep(time.Second * 1) // time for nats to start
 	srv := NewServer(NatsClientOpt{
-		Addr:          fmt.Sprintf("nats://127.0.0.1:%d", TEST_PORT),
+		Addr:          URL,
 		ReconnectWait: time.Second * 2,
 		MaxReconnects: 100,
 	}, Config{
